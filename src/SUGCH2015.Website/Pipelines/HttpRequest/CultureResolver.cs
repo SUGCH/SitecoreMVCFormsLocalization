4dcb9e8e459c3052a860251f0044a37b991c83c9
Kevin Brechbühl <mail@aquasonic.ch>
2015-04-13 22:08:58 +0200

    Set current culture in pipeline processor.

---
+++ src/SUGCH2015.Website/Pipelines/HttpRequest/CultureResolver.cs
@@ -0,0 +1,21 @@
+﻿namespace SUGCH2015.Website.Pipelines.HttpRequest
+{
+    using System.Globalization;
+    using System.Threading;
+    using Sitecore.Pipelines.HttpRequest;
+
+    public class CultureResolver : HttpRequestProcessor
+    {
+        public override void Process(HttpRequestArgs args)
+        {
+            if (Sitecore.Context.Language == null || Sitecore.Context.Database == null || string.IsNullOrEmpty(Sitecore.Context.Language.Name)) return;
+
+            var language = Sitecore.Context.Database.GetItem("/sitecore/system/Languages/" + Sitecore.Context.Language.Name);
+            if (language == null) return;
+            
+            var culture = new CultureInfo(language["regional iso code"]);
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+    }
+}
