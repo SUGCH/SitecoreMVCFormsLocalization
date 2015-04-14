6b57c256deef95a055e959748bf2c4ffc0fab231
Kevin Brechbühl <mail@aquasonic.ch>
2015-04-14 17:58:28 +0200

    Rename controller to only have one for future implementations.

--- src/SUGCH2015.Website/Controllers/FormController.cs
+++ src/SUGCH2015.Website/Controllers/FormController.cs
@@ -3,9 +3,9 @@
     using System.Web.Mvc;
     using Models;
 
-    public class TraditionalController : Controller
+    public class FormController : Controller
     {
-        public ActionResult Index(TraditionalViewModel model = null)
+        public ActionResult Traditional(TraditionalViewModel model = null)
         {
             return this.View(model ?? new TraditionalViewModel());
         }
