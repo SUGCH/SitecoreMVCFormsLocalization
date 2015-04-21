c17954cc6e57a46f7932674adadf45fc48fb786d
Tobias Studer <studert@live.com>
2015-04-21 21:36:24 +0200

    Add Sitecore translations

--- src/SUGCH2015.Website/Controllers/FormController.cs
+++ src/SUGCH2015.Website/Controllers/FormController.cs
@@ -10,6 +10,11 @@
             return this.View(model ?? new TraditionalViewModel());
         }
 
+        public ActionResult Dictionary(DictionaryViewModel model = null)
+        {
+            return this.View(model ?? new DictionaryViewModel());
+        }
+
         public ActionResult ServerSideValidation(ServerSideValidationViewModel model = null)
         {
             return this.View(model ?? new ServerSideValidationViewModel());
---
+++ src/SUGCH2015.Website/Models/DictionaryViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SUGCH2015.Website.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class DictionaryViewModel
+    {
+        [Required]
+        public string PropertyName { get; set; }
+    }
+}
